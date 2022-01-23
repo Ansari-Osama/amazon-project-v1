b0ecd2a888feae68d70332414e85acb800a8f83d
Osama Ansari <ansariosama192@gmail.com>
2022-01-23 22:42:57 +0530

    Updated

--- src/Payment.js
+++ src/Payment.js
@@ -4,40 +4,44 @@ import CheckoutProduct from './CheckoutProduct';
 import { useStateValue } from './StateProvider';
 
 function Payment() {
-    const[{cart, user}, dispatch] = useStateValue();
+    const [{ cart, user }, dispatch] = useStateValue();
 
     return (
         <div className='payment'>
             <div className='payment--container'>
                 {/* Delivery Address */}
                 <div className='payment--section'>
-                    <h3>Delivery Address: </h3>
+                    <div className='payment--title'>
+                        <h3>Delivery Address: </h3>
+                    </div>
                     <div className='payment--address'>
                         <p>{user?.email}</p>
                         <p>Flat No 02, Hiranandani</p>
                         <p>Andheri, India</p>
                     </div>
                 </div>
-
                 {/* Cart Review */}
                 <div className='payment--section'>
-                    <h3>Review Your Items in Cart: </h3>
-                    <div className='payment--item'>
-                        
-                    {cart.map( item => 
-                        <CheckoutProduct 
-                            id = {item.id}
-                            title = {item.title}
-                            image = {item.image}
-                            price = {item.price}
-                        />
-                    )}
+                    <div className='payment--title'>
+                        <h3>Review Your Items in Cart: </h3>
+                    </div>
+                    <div className='payment--items'>
+
+                        {cart.map(item =>
+                            <CheckoutProduct
+                                id={item.id}
+                                title={item.title}
+                                image={item.image}
+                                price={item.price}
+                            />
+                        )}
                     </div>
                 </div>
-                
                 {/* Payment Gateway */}
                 <div className='payment--section'>
-                    <h3>Payment Method: </h3>
+                    <div className='payment--title'>
+                        <h3>Payment Method: </h3>
+                    </div>
                     <div className='payment--details'>
                         {/* Stripe Secret Code */}
                     </div>
